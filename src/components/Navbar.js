7b77d3cc0249b5b0442b28a957184dd649e02e3c
srikanth159 <150225151+srikanth159@users.noreply.github.com>
2025-02-24 15:59:59 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,25 +1,3 @@
-// import React from 'react';
-// import { Link } from 'react-router-dom';
-
-
-// const Navbar = () => {
-//   return (
-//     <div>
-//       <h1>Navbar</h1>
-//       <ul>
-//         <li><Link to="/">Login</Link></li>
-//         <li><Link to="/dashboard">Dashboard</Link></li>
-//         <li><Link to="/users">Users</Link></li>
-//         <li><Link to="/videos">ideos</Link></li>
-//         <li><Link to="/categories">Categories</Link></li>
-//         {/* Add other navigation links as needed */}
-//       </ul>
-//     </div>
-//   );
-// };
-
-// export default Navbar;
-// src/components/Navbar.js
 import React from 'react';
 import { Link } from 'react-router-dom';
 
