85aacfa588416ed112dd7030cdb9795c3d77fb04
srikanth159 <150225151+srikanth159@users.noreply.github.com>
2025-02-24 16:08:35 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,28 +1,3 @@
-// import React from 'react';
-// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-// import Dashboard from './components/Dashboard';
-// import Users from './components/Users';
-// import Videos from './components/Videos';
-// import Categories from './components/Categories';
-// import 'bootstrap/dist/css/bootstrap.min.css';
-// import Login from './components/Login';
-
-// const App = () => {
-//     return (
-        
-//         <Router>
-//             <Routes>
-//                 <Route path='/' element ={<Login/>}/>
-//                 <Route path="/dashboard" element={<Dashboard />} />
-//                 <Route path="/users" element={<Users />} />
-//                 <Route path="/videos" element={<Videos />} />
-//                 <Route path="/categories" element={<Categories />} />
-//             </Routes>
-//         </Router>
-//     );
-// };
-
-// export default App;
 import React from 'react';
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
